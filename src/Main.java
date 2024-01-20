c340605e014bfb2e5b0f01b62877fe052f0ef6dd
Kislay Shekhar <kislayshekhar2@gmail.com>
2024-01-20 13:52:15 +0530

    arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1=new Student("Kislay","cse",001,790);
-        Student[] studentArray = new Student[20];
+        Student s1=new Student("Nikhil","cse",001,790);
+        Student s2=new Student("Raagul","cs",123,123);
+        Student s3=new Student("Bharat","cs",123,123);
+
+        ArrayList studentList=new ArrayList();
+        studentList.add(s1);
+        studentList.add(s2);
+        studentList.add(s3);
+
+       // for(int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList);
+       // }
+
+
+        //        Student[] studentArray = new Student[20];
 
         // Populate the array with Student objects
        /* for (int i = 0; i < studentArray.length; i++) {
@@ -12,8 +26,6 @@ public class Main {
             System.out.println(studentArray[i].dept);
             System.out.println(studentArray[i].phNo + "\n");
         }*/
-
-        System.out.println(s1);
 
     }
     }
